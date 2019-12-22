10d3e8892d32455fcc8b9f12a54660c5fa7f0382
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-12-22 06:05:20 +0300

    Add files via upload

--- asm2.asm
+++ asm2.asm
@@ -1,4 +1,28 @@
+.data
+MyByte db 10, 11, 12, 13
+MyByte2 dq 12h, 34h
+MyByte3 dd 38h, 20h
+MyByte5 dw 1234h, 5678h
+MyByte4 db 12
+FucksGiven db 0
+notlar db 69h, 87h, 96h, 45h, 75h
+enyuksek db ?
+
 .code
+
+TestFunction proc
+	lea rbx, MyByte
+	push 0h
+	push 0h
+		;add [MyByte], 76
+		;mov rax, qword ptr[MyByte3]
+	push word ptr [MyByte5]
+	mov rax, 123h
+	push ax
+	pop rbx
+	lea rdx, enyuksek
+TestFunction endp
+
 Hello proc
 	mov cx,5
 	mov ah, 0eh
@@ -26,4 +50,15 @@ TheBiggest proc
 	ret
 TheBiggest endp
 
+Max proc
+	cmp rcx, rdx
+	jge cDonder
+	mov rax, rdx
+	jmp endDonder
+	cDonder:
+		mov rax,rcx
+	endDonder:
+		nop
+	ret
+Max endp 
 end
